e4c5d654892716abad901b1e759aa7a67e0db4e2
quan2910 <ngtruongquan@gmail.com>
2024-04-01 23:48:23 +0700

    savee

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const Layout = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <Outlet></Outlet>
       <Footer />
-    </div>
+    </>
   );
 };
 
