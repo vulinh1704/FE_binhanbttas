8523fccfc71a2491101055d950bc4642631a0ded
quan29 <ngtruongquan@gmail.com>
2024-04-23 16:36:02 +0700

    saksa

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./i18n";
 
 const queryClient = new QueryClient();
 
